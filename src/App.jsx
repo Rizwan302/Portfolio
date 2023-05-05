0fa6a72d49081b0c0eb62ec36ce631528a751fd0
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-05 22:47:29 +0500

    Convert to css to scss file App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+import './App.scss'
+import Topbar from './components/topbar/Topbar'
+import Intro from './components/intro/Intro'
+import Portfolio from './components/portfolio/Portfolio'
+import Works from './components/work/Works'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
 
 function App() {
 
   return (
     <div className='app'>
-      
+      <Topbar/>
+      <div className="section">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+
+      </div>
     </div>
   )
 }
