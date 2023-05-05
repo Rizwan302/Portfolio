baf144175a25a57ea3d81a61636f2518b5f032bb
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-06 00:57:31 +0500

    Add icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-// import { useState } from 'react'
-import './App.scss'
-import Topbar from './components/topbar/Topbar'
-import Intro from './components/intro/Intro'
-import Portfolio from './components/portfolio/Portfolio'
-import Works from './components/work/Works'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
+import React, { useState } from "react"; 
+import "./App.scss";
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/work/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 function App() {
-
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className='app'>
-      <Topbar/>
+    <div className="app">
+      <Topbar open={menuOpen} close={setMenuOpen} name={"Hello"}/>
       <div className="section">
-        <Intro/>
-        <Portfolio/>
-        <Works/>
-        <Testimonials/>
-        <Contact/>
-
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import './topbar.scss'
+import './topbar.scss';
+import MarkunreadIcon from '@mui/icons-material/Markunread';
+import PersonIcon from '@mui/icons-material/Person';
 
-export default function Topbar() {
+export default function Topbar({name, open, close}) {
   return (
-    <div className='topbar'>
-      hello
+    <div className={"topbar " + (open && 'active')}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className='logo'>logo</a>
+
+          <div className="itemContainer">
+          <PersonIcon className='icon' />
+          <span>+92 310 1599536</span>
+          <p>{name}</p>
+          </div>
+
+          <div className="itemContainer">
+          <MarkunreadIcon className='icon' />
+          <span>+92 310 1599536</span>
+          </div>
+
+
+        {/* <h1>Hello</h1> */}
+        </div>
+        <div className="right">
+          <div className="humburger" onClick={()=> close(!open)}>
+            <span className="line"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
